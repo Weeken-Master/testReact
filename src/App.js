903dee0eab66410909726ffad583637ccd17c058
Weeken-Master <86185473+Weeken-Master@users.noreply.github.com>
2022-02-16 17:07:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Mình là Tuần  mình chỉ test deloy
         </a>
       </header>
     </div>
